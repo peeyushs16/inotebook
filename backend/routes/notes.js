e2978ce4e44f040c1b2e62c8556e57bda88cbe78
peeyushs16 <peeyushs16@gmail.com>
2023-05-18 22:16:22 +0530

    Setup frontend with backend

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const fetchuser = require('../middleware/fetchuser');
 const { body, validationResult } = require('express-validator');
 
+// fetch all notes of a user
 router.get('/fetchalldata', fetchuser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id })
@@ -15,6 +16,7 @@ router.get('/fetchalldata', fetchuser, async (req, res) => {
     }
 })
 
+// create notes
 router.post('/addNotes', fetchuser, [
     body('title', 'Enter a title').isLength({ min: 3 }),
     body('description', 'Enter description').isLength({ min: 6 }),
@@ -37,4 +39,55 @@ router.post('/addNotes', fetchuser, [
     }
 
 })
+
+// update note of user
+
+router.put('/updateNotes/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+
+    try {
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        const checkNote = await Note.findById(req.params.id);
+        if (!checkNote) {
+            return res.status(404).send("Note not found");
+        }
+        if (checkNote.user.toString() !== req.user.id) {
+            return res.status(401).send("Unauthorised");
+        }
+
+        const result = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+
+        res.json(result);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+
+
+})
+
+
+// delete notes
+router.delete('/deleteNote/:id', fetchuser, async (req, res) => {
+
+    try {
+        const checkNote = await Note.findById(req.params.id);
+        if (!checkNote) { return res.status(404).send("Note not found") }
+
+        if (checkNote.user.toString() !== req.user.id) { return res.status(401).send("Unauthorised") }
+
+        const result = await Note.findByIdAndDelete(req.params.id);
+
+        res.json({ "Success": "Note delete successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+
+})
+
 module.exports = router;
