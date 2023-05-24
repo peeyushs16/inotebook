de4663dcee0282a53ef2c1242c8a0a30eb8b43f5
peeyushs16 <peeyushs16@gmail.com>
2023-05-24 11:58:57 +0530

    Front view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <h1>This is my react app</h1>
+    <>
+      <Router>        
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/About" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
